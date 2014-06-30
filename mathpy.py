b110eb29f26dcd6f31fca6db6d94d0848d376314
Kian Moretz <meecoder@users.noreply.github.com>
2014-06-30 18:45:30 -0400

    Add multiproblem and stats to sub, multi, divis, exp

--- mathpy.py
+++ mathpy.py
@@ -46,94 +46,146 @@ def mathpyrun():
 		print(" ")
 		print(percent + '% correct')
 	elif(mathtypei == 2):
-		n1 = randint(0, 1000)
+		while(i <= mathnum):
+			n1 = randint(0, 1000)
 	
-		n2 = randint(0, n1)
+			n2 = randint(0, n1)
 	
-		print("subtract:")
-		print(n1)
-		print(n2)
-		
-		ans = input("answer: ")
-		
-		realans = n1 - n2
-		
-		ansint = int(ans)
-		
-		if (ansint == realans):
-			print("Correct!")
-		else:
-			print("Incorrect. The answer is:")
-			print(realans)
+			print("subtract:")
+			print(n1)
+			print(n2)
+			
+			ans = input("answer: ")
+			
+			realans = n1 - n2
+				
+			ansint = int(ans)
+			
+			if (ansint == realans):
+				print("Correct!")
+				correct += 1
+			else:
+				print("Incorrect. The answer is:")
+				print(realans)
+				incorrect += 1
+			i += 1
+		print("------STATISTICS------")
+		print("Correct:")
+		print(correct)
+		print("  ")
+		print("Incorrect:")
+		print(incorrect)
+		percent = (float(correct) / float(mathnum) * 100.0)
+		percent = str(percent)
+		print(" ")
+		print(percent + '% correct')
 	elif(mathtypei == 3):
-	
-		n1 = randint(0, 1000)
-		
-		n2 = randint(0, 99)
-		
-		print("multiply:")
-		print(n1)
-		print(n2)
-		
-		ans = input("answer: ")
-		
-		realans = n1 * n2
-		
-		ansint = int(ans)
-		
-		if (ansint == realans):
-			print("Correct!")
-		else:
-			print("Incorrect. The answer is: ")
-			print(realans)
+		while(i <= mathnum):
+			n1 = randint(0, 1000)
+			
+			n2 = randint(0, 99)
+			
+			print("multiply:")
+			print(n1)
+			print(n2)
+			
+			ans = input("answer: ")
+			
+			realans = n1 * n2
+			
+			ansint = int(ans)
+			
+			if (ansint == realans):
+				print("Correct!")
+				correct += 1
+			else:
+				print("Incorrect. The answer is: ")
+				print(realans)
+				incorrect += 1
+			i += 1
+		print("------STATISTICS------")
+		print("Correct:")
+		print(correct)
+		print("  ")
+		print("Incorrect:")
+		print(incorrect)
+		percent = (float(correct) / float(mathnum) * 100.0)
+		percent = str(percent)
+		print(" ")
+		print(percent + '% correct')
 	elif(mathtypei == 4):
-		pre1 = randint(1, 50)
-		pre2 = randint(1, 50)
-		n2 = pre1 * pre2
-		pre = [pre1, pre2]
-		n1 = choice(pre)
-		
-		print("divide first number by second number:")
-		print(n2)
-		print(n1)
-		
-		ans = input("answer: ")
-		ansint = int(ans)
-		
-		if(n1 == pre1):
-			realans = pre2
-		else:
-			realans = pre1
+		while(i <= mathnum):
+	
+			pre1 = randint(1, 50)
+			pre2 = randint(1, 50)
+			n2 = pre1 * pre2
+			pre = [pre1, pre2]
+			n1 = choice(pre)
 			
-		
-		
-		 
-		
-		if (ansint == realans):
-			print("Correct!")
-		else:
-			print("Incorrect. The answer is: " )
-			print(realans)
+			print("divide first number by second number:")
+			print(n2)
+			print(n1)
+			
+			ans = input("answer: ")
+			ansint = int(ans)
+			
+			if(n1 == pre1):
+				realans = pre2
+			else:
+				realans = pre1
+			
+			if (ansint == realans):
+				print("Correct!")
+				correct += 1
+			else:
+				print("Incorrect. The answer is: " )
+				print(realans)
+				incorrect += 1
+			i += 1
+		print("------STATISTICS------")
+		print("Correct:")
+		print(correct)
+		print("  ")
+		print("Incorrect:")
+		print(incorrect)
+		percent = (float(correct) / float(mathnum) * 100.0)
+		percent = str(percent)
+		print(" ")
+		print(percent + '% correct')
 	elif(mathtypei == 5):
-		n1 = randint(0, 10)
+		while(i <= mathnum):
+			n1 = randint(0, 10)
+				
+			n2 = randint(0, 10)
+				
+			print("first number to second number power:")
+			print(n1)
+			print(n2)
+				
+			ans = input("answer: ")
 			
-		n2 = randint(0, 10)
+			realans = n1 ** n2
+				
+			ansint = int(ans)
 			
-		print("first number to second number power:")
-		print(n1)
-		print(n2)
-			
-		ans = input("answer: ")
-		
-		realans = n1 ** n2
-			
-		ansint = int(ans)
-		
-		if (ansint == realans):
-			print("Correct!")
-		else:
-			print("Incorrect. The answer is: ")
-			print(realans)
+			if (ansint == realans):
+				print("Correct!")
+				correct += 1
+			else:
+				print("Incorrect. The answer is: ")
+				print(realans)
+				incorrect += 1
+			i += 1
+		print("------STATISTICS------")
+		print("Correct:")
+		print(correct)
+		print("  ")
+		print("Incorrect:")
+		print(incorrect)
+		percent = (float(correct) / float(mathnum) * 100.0)
+		percent = str(percent)
+		print(" ")
+		print(percent + '% correct')
 	else:
 		print("Please type 1, 2, 3, or 4.")
 		mathpyrun()	
